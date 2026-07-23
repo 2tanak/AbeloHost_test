6b999f415d529afc5aea65a9be65f381ac766a79
Kanat <2tanak@mail.ru>
2026-07-24 03:20:10 +0500

    Designing the menu

--- app/templates_c/e869dc9c0d2d22c898b582a1b87f05bda2b33c3a_0.file.header.tpl.php
+++ app/templates_c/e869dc9c0d2d22c898b582a1b87f05bda2b33c3a_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.5.7, created on 2026-07-23 09:32:26
+/* Smarty version 4.5.7, created on 2026-07-23 22:19:22
   from '/var/www/app/app/templates/header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.5.7',
-  'unifunc' => 'content_6a61dfaa1ae0a8_72883386',
+  'unifunc' => 'content_6a62936a1d3d89_56009027',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e869dc9c0d2d22c898b582a1b87f05bda2b33c3a' => 
     array (
       0 => '/var/www/app/app/templates/header.tpl',
-      1 => 1784792546,
+      1 => 1784845159,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6a61dfaa1ae0a8_72883386 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6a62936a1d3d89_56009027 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,7 +33,29 @@ function content_6a61dfaa1ae0a8_72883386 (Smarty_Internal_Template $_smarty_tpl)
 </head>
 <body>
 <header>
-    <!-- Здесь будет шапка сайта, логотип, навигация -->
+    <section id="navigate">
+		
+		<div class="navigate">
+				<input class="side-menu" type="checkbox" id="side-menu" />
+				<label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+
+				<nav class="nav">
+					<ul>
+						<li class='href_hover'><a href="#">Главная</a></li>
+					
+						<li class='href_hover'>
+						  <a href="#">Категория</a>
+						 </li>
+						
+						<li class='href_hover'>
+						  <a href="#">Статьи</a>
+						 </li>
+						
+					</ul>
+				</nav>
+			</div>
+		
+	</section>
 </header>
 <?php }
 }
