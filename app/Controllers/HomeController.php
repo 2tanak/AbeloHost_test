0d4baea9fdd54e4530f546bea0ff592e824e2924
Kanat <2tanak@mail.ru>
2026-07-23 10:38:09 +0500

    steo-2_ create patern mvc

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Models\Article;
+
+class HomeController
+{
+    
+    /**
+     * Точка входа для роутера
+     */
+    public function index(): void
+    {
+        
+        echo "PATERN MVC ";
+    }
+}
