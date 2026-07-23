a7ad07139ec2f428f544cb08b15b43ddbca52e20
Kanat <2tanak@mail.ru>
2026-07-23 10:53:10 +0500

    create patern template

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,15 +6,25 @@ namespace App\Controllers;
 
 use App\Models\Article;
 
-class HomeController
+class HomeController extends BaseController
 {
-    
+    public function setContent(): void 
+    {
+		$content ='content';
+       
+		
+		
+        $this->smarty->assign('categories', $content);
+        
+        $this->content = $this->smarty->fetch('content.tpl');
+    }
+
     /**
      * Точка входа для роутера
      */
     public function index(): void
     {
-        
-        echo "PATERN MVC ";
+        $this->setContent();
+        $this->output(); 
     }
 }
